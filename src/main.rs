d543445f644caad3f780cccb4f5b452dd1ef5b26
Huang yao ting <a0916996002@gmail.com>
2022-11-28 14:55:21 +0800

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ fn main() {
     io::stdin().read_line(&mut 變數)
 
         io::Result 
-        返回值: Ok + 結果值
-        Err + 失敗原因
-        expect() : 若返回值為 Err，則中斷當前運作，且把傳入的字串顯示出來
-        反之，Ok 則是提取 附加的值，作為結果返回給使用者
+        返回值: 
+                Ok + 結果值
+                Err + 失敗原因
+                expect() : 若返回值為 Err，則中斷當前運作，且把傳入的字串顯示出來
+                反之，Ok 則是提取 附加的值，作為結果返回給使用者
     */
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("讀取失敗");
