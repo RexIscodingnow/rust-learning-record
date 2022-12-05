c73eac84ee7c0ce0a8de0a10686054f0087d7a33
Huang yao ting <a0916996002@gmail.com>
2022-12-05 11:28:57 +0800

    變數: 字元、數組、Tuple

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,10 +1,83 @@
+// use std::io::stdin;
+
+// const MAX_POINTS: u32 = 100_000;   // 常數
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    // 常數(常量)，不可變
-    let constVar = 4152;
-    println!("{}", constVar);
+    // 常數(常量)，不可變 (預設不可變動)
+    // let constVar = 4152;
+    // println!("{}", constVar);
 
-    let mut var = 0;
-    println!("{}", var);
+
+    // 在 let 後面加上 mut 就可以變動
+    // let mut var = 0;
+    // println!("{}", var);
+
+    // 隱藏 (shadowing)
+    // let x = 5;
+    // println!("{}", x);
+
+    // let x = x + 5;
+    // println!("{}", x);
+    
+    // let x = x + 3;
+    // println!("{}", x);
+
+    // let space = "     ";
+    // println!("{}", space);
+    // let space =  space.len();
+    // println!("{}", space);
+
+    // 浮點數
+    // let x = 32.45;
+    // let x: f32 = 6.45;
+    
+    // 基本運算
+    // let mut num = 54;
+    // let mut temp = num;
+    // println!("原先數值 num = {}", num);
+    // temp = num;
+    // num *= 123;
+    // println!("{} x 123 = {}", temp, num);
+    // temp = num;
+    // num /= 88;
+    // println!("{} / 123 = {}", temp, num);
+    // temp = num;
+    // num -= 456;
+    // println!("{} - 123 = {}", temp, num);
+    // temp = num;
+    // num += 1000;
+    // println!("{} + 123 = {}", temp, num);
+
+    // 布林值
+    // let t: bool = true;
+    // let f = false;
+
+
+    // 字元
+    // let x = 'X';
+    // let y = 'Z';
+    // let emoji = '😂';
+
+    // println!("{}", x);
+    // println!("{}", y);
+    // println!("{}", emoji);
+
+    // Tuple
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // let (x, y, z) = tup;
+    // println!("x = {}, y = {}, z = {}", x, y, z);
+    // println!("x = {}, y = {}, z = {}", tup.0, tup.1, tup.2);
+
+    // 數組
+    // let a = [1, 2, 3, 4, 5];
+
+    // let one = a[0];
+    // let two = a[1];
+    // println!("{}, {}", one, two);
+    
+    // println!("{}", a[4]);   // 超過索引值範圍，不會抱錯
+
+    
 }
